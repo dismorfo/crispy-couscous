1b3da2d562422eac532410c513568fbb022bf8de
Crystal Chu <cchu618@gmail.com>
2022-02-07 16:29:16 -0500

    react iframe and initial formatting

--- src/pages/embedded-book/index.jsx
+++ src/pages/embedded-book/index.jsx
@@ -1,10 +1,26 @@
 import { Link, useParams } from 'react-router-dom';
+import Iframe from 'react-iframe'
 
+function Book(props) { 
+  const { identifier = '' } = props;
+
+  console.log(identifier);
+
+  const url = `https://objective-johnson-b22e71.netlify.app/books/${identifier}/1`
+  return (
+    <Iframe src={url}
+            width="100%"
+            height="750px"
+            id="aBook"
+            display="block"
+            />
+  )
+}
 export default function EmbeddedBook() {
   let { identifier } = useParams();
   return (
     <>
-      <p>Book {identifier} here.</p>
+      <Book identifier={identifier}/>
       <p><Link to="/">Go to the home page</Link></p>
     </>
   );
