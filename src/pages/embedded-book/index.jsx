fef4a2850c93b6ed3d78122d2949b7b5653a28ff
Crystal Chu <cchu618@gmail.com>
2022-02-09 11:19:31 -0500

    added Book component

--- src/pages/embedded-book/index.jsx
+++ src/pages/embedded-book/index.jsx
@@ -1,27 +1,10 @@
-import { Link, useParams } from 'react-router-dom';
-import Iframe from 'react-iframe'
+import { useParams } from 'react-router-dom';
 
-function Book(props) { 
-  const { identifier = '' } = props;
-
-  console.log(identifier);
-
-  const url = `https://objective-johnson-b22e71.netlify.app/books/${identifier}/1`
-  return (
-    <Iframe src={url}
-            width="100%"
-            height="750px"
-            id="aBook"
-            display="block"
-            />
-  )
-}
 export default function EmbeddedBook() {
   let { identifier } = useParams();
   return (
     <>
       <Book identifier={identifier}/>
-      <p><Link to="/">Go to the home page</Link></p>
     </>
   );
 }
